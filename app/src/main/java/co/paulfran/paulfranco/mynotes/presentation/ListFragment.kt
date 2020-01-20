ea165a03eb0ebd1985e4703fa95206e5abaf3cbd
Paul Franco <paulfranco@me.com>
2020-01-19 18:47:42 -0800

    Added Navigation

---
+++ app/src/main/java/co/paulfran/paulfranco/mynotes/presentation/ListFragment.kt
@@ -0,0 +1,40 @@
+package co.paulfran.paulfranco.mynotes.presentation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+
+import co.paulfran.paulfranco.mynotes.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addNote.setOnClickListener{ goToNoteDetails() }
+    }
+
+    private fun goToNoteDetails(id: Long = 0L){
+        val action: NavDirections = ListFragmentDirections.goToNote(id)
+        Navigation.findNavController(notesListView).navigate(action)
+    }
+
+
+}
